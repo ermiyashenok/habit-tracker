721056cf15fe336263e191132f119067f87d82c9
ermiyashenok <ermiyashenok44@gmail.com>
2026-07-14 22:30:53 +0300

    feat: add firebase secrets template

---
+++ maximize/lib/firebase_secrets.template.dart
@@ -0,0 +1,5 @@
+class FirebaseSecrets {
+  static const String webApiKey = 'YOUR_WEB_API_KEY';
+  static const String androidApiKey = 'YOUR_ANDROID_API_KEY';
+  static const String iosApiKey = 'YOUR_IOS_API_KEY';
+}
